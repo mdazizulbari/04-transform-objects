fce7309a0f32f75784b7a0fb77e20b5b7b2ae416
smabari <mdazizulbarishafin@gmail.com>
2025-11-17 19:22:52 +0600

    removed cube

--- src/script.js
+++ src/script.js
@@ -9,28 +9,28 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
-// Position
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-// mesh.position.normalize()
-mesh.position.set(0.7, -0.6, 1);
+// // Position
+// // mesh.position.x = 0.7
+// // mesh.position.y = -0.6
+// // mesh.position.z = 1
+// // mesh.position.normalize()
+// mesh.position.set(0.7, -0.6, 1);
 
-// Scale
-// mesh.scale.x=2
-// mesh.scale.y=0.5
-// mesh.scale.z=0.5
-mesh.scale.set(2, 0.5, 0.5);
+// // Scale
+// // mesh.scale.x=2
+// // mesh.scale.y=0.5
+// // mesh.scale.z=0.5
+// mesh.scale.set(2, 0.5, 0.5);
 
-// Rotation
-mesh.rotation.reorder("YXZ")
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+// // Rotation
+// mesh.rotation.reorder("YXZ")
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(3);
@@ -53,7 +53,7 @@ camera.position.z = 3;
 // camera.position.x = 1
 scene.add(camera);
 
-camera.lookAt(mesh.position)
+// camera.lookAt(mesh.position)
 // console.log(mesh.position.length());
 // console.log(mesh.position.distanceTo(camera.position));
 
