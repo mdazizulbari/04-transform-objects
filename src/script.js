1767c04b180fb9d3013c3b3342b3e0570f1bb88b
smabari <mdazizulbarishafin@gmail.com>
2025-11-17 19:01:14 +0600

    using axeshelper

--- src/script.js
+++ src/script.js
@@ -20,6 +20,10 @@ scene.add(mesh)
 // mesh.position.normalize()
 mesh.position.set(0.7,-0.6,1)
 
+// Axes helper
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+
 /**
  * Sizes
  */
@@ -33,6 +37,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
 scene.add(camera)
 
 console.log(mesh.position.length())
