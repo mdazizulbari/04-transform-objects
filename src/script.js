39c4e61352300ce90ae5c955f2ca79758e49bff2
smabari <mdazizulbarishafin@gmail.com>
2025-11-17 19:07:54 +0600

    formating

--- src/script.js
+++ src/script.js
@@ -1,31 +1,31 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Position
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = 1
 // mesh.position.normalize()
-mesh.position.set(0.7,-0.6,1)
+mesh.position.set(0.7, -0.6, 1);
 
 // Scale
 // mesh.scale.x=2
 // mesh.scale.y=0.5
 // mesh.scale.z=0.5
-mesh.scale.set(2,0.5,0.5)
+mesh.scale.set(2, 0.5, 0.5);
 
 // Axes helper
 // const axesHelper = new THREE.AxesHelper(3)
@@ -35,27 +35,27 @@ mesh.scale.set(2,0.5,0.5)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
 // camera.position.y = 1
 // camera.position.x = 1
-scene.add(camera)
+scene.add(camera);
 
-console.log(mesh.position.length())
-console.log(mesh.position.distanceTo(camera.position))
+console.log(mesh.position.length());
+console.log(mesh.position.distanceTo(camera.position));
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
